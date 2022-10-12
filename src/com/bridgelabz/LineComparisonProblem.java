9ad721281ffd46c3caa8d5f4fa044be51eba068c
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-12 10:10:02 +0530

    Added : Length of Line

--- src/com/bridgelabz/LineComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -7,13 +7,13 @@ public class LineComparisonProblem
     void lengthOfLine()
     {
         System.out.println("Welcome To Line Comparison Computation Program");
-        int x1=10,y1=20,x2=30,y2=40;
-        double lengthOfLine =Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+        int x1=10,y1=20,x2=30,y2=40; // Local variables
+        double lengthOfLine =Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2)); // sqrt and Power function
         System.out.println("Length of Line : " + lengthOfLine);
     }
     public static void main(String[] args)
     {
-        LineComparisonProblem line = new LineComparisonProblem();
-        line.lengthOfLine();
+        LineComparisonProblem line = new LineComparisonProblem(); // Object created
+        line.lengthOfLine(); // call method
     }
 }
