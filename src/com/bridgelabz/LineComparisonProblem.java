ba2fc18692839a5f153ea4f5eef561f9688eba1a
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-12 23:25:21 +0530

    Added : Equal and Compare

--- src/com/bridgelabz/LineComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -1,4 +1,4 @@
-// UC-2 Line Comparison Problem equals lines
+// UC-4 Line Comparison Problem equal and Compare
 
 package com.bridgelabz;
 
@@ -11,7 +11,7 @@ public class LineComparisonProblem
     void lengthOfLine()
     {
         System.out.println("Line Comparison Program two lines are the equal");
-        int x1=10,y1=20,x2=30,y2=40; // Local variables
+        int x1=10,y1=20,x2=30,y2=50; // Local variables
         int x3=10,y3=20,x4=30,y4=40;
         lengthOfLine1 =Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2)); // sqrt and Power function
         System.out.println("Length of First Line : " + lengthOfLine1);
@@ -25,11 +25,20 @@ public class LineComparisonProblem
         else
             System.out.println("Lines are not equals");
     }
+    void compareTo()
+    {
+        if(Double.compare(lengthOfLine1,lengthOfLine2)==0) // Compare To function
+            System.out.println("equals");
+        else if (Double.compare(lengthOfLine1,lengthOfLine2)>0)
+            System.out.println("line 1 Greater then line 2");
+        else
+            System.out.println("line 1 lesser then line 2");
+    }
     public static void main(String[] args)
     {
         LineComparisonProblem line = new LineComparisonProblem(); // Object created
         line.lengthOfLine(); // call method
-        line.equal();
-
+        line.equal(); //equals
+        line.compareTo();
     }
 }
